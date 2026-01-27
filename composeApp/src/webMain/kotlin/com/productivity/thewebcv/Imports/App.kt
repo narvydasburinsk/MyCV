5b9ac910b4f9bf166d3ac0c6a048886003c0732f
narvydasburinsk <narvydas.burinsk@gmail.com>
2026-01-27 11:08:03 +0100

    Create App.kt

---
+++ composeApp/src/webMain/kotlin/com/productivity/thewebcv/Imports/App.kt
@@ -0,0 +1,24 @@
+package com.productivity.thewebcv.Imports
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
